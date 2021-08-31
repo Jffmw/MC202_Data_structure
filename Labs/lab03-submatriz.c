5d9e390658d23b001770b238a091f6d2053e8f74
Jffmw <joao.francisco.doc@gmail.com>
2021-08-31 13:02:21 -0300

    lab 3 concluído com casos 2 e 3 não funcionando

--- Labs/lab03-submatriz.c
+++ Labs/lab03-submatriz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    // declaração das variáveis para criação da matriz e da soma desejada
+    // declaracao das variáveis para criação da matriz e da soma desejada
     int n, m, i, j, s;
   
     scanf("%d", &n);
@@ -21,21 +21,45 @@ int main() {
         }    
     }
     
-    // início dos testes com as submatrizes
+    // inicio dos testes com as submatrizes
+    int icont, jcont, contagem;
+    int tamanho_submatriz, tamanho_matriz;
+    int soma, inicio_submatriz_linha, inicio_submatriz_coluna;
 
-
-    for (i=0; i < n; i++) 
+    if (n >= m)
+        tamanho_matriz = m;
+    else
+        tamanho_matriz = n;
+    
+    for(tamanho_submatriz = 1; tamanho_submatriz <  tamanho_matriz ; tamanho_submatriz++) 
     {
-        for (j=0; j < m; j++) 
+        contagem = tamanho_matriz - tamanho_submatriz + 1;
+        for (icont = 0; icont < contagem; icont++) 
         {
-            if(M[i][j] == s)
+            for (jcont = 0; jcont < contagem; jcont++) 
             {
-                printf("submatriz 1 x 1 em [%d,%d]", i+1, j+1);
-                return 0;
+                soma = 0;
+                inicio_submatriz_linha = 1 + icont;
+                inicio_submatriz_coluna = 1 + jcont; 
+ 
+                for (i = 0; i < tamanho_submatriz; i++) 
+                {
+
+                    for (j = 0; j < tamanho_submatriz; j++) 
+                    {
+                        soma +=  M[i+icont][j+jcont];
+                    }
+                    
+                }
+
+                if (soma == s) 
+                {
+                    printf("submatriz %d x %d em [%d,%d]\n", i, i, inicio_submatriz_linha, inicio_submatriz_coluna); 
+                    return 0;
+                }
             }
         }
     }
-    
-
+    printf("nao ha'\n");
     return 0;
 }
